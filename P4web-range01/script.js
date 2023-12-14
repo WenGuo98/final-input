4a725ea0baad76a6af82d268cbfb89543faa87e5
WenGuo98 <147080449+WenGuo98@users.noreply.github.com>
2023-12-13 19:36:23 -0800

    f02

--- P4web-range01/script.js
+++ P4web-range01/script.js
@@ -1,8 +1,8 @@
 function updateOpacity(value) {
-    const opacity = value / 100;
-    document.querySelector('.text-container').style.opacity = opacity;
-  }
+  const opacity = value / 100;
+  document.querySelector('.text-container').style.opacity = opacity;
+}
 
-  function navigateToNextPage() {
-    window.location.href = 'file:///Users/artemis/Desktop/Web%20to%20Print/F/final%20web/final-input/P4web-color01/index.html';
-  }
+function navigateToNextPage() {
+  window.location.href = 'P4web-color01/index.html';
+}
--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var inputField = document.getElementById('fname');
+    var inputField = document.getElementById('fname');
 
-  inputField.addEventListener('input', function () {
-    var inputValue = inputField.value.trim().toLowerCase();
+    inputField.addEventListener('input', function () {
+      var inputValue = inputField.value.trim().toLowerCase();
 
-    // 使用相对路径跳转到下一页
-    if (inputValue === 'input') {
-      window.location.href = 'P4web-range01/index.html';
-    }
-  });
+      if (inputValue === 'input') {
+        window.location.href = 'P4web-range01/index.html';
+      }
+    });
 });
