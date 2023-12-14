55c05dd13748c47dc333ae2200c4faa8d1b6daa3
WenGuo98 <147080449+WenGuo98@users.noreply.github.com>
2023-12-14 09:46:17 -0800

    f000

--- P4web-restart01/script.js
+++ P4web-restart01/script.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   var resetButton = document.getElementById('resetButton');
 
   resetButton.addEventListener('click', function () {
-    window.location.href = '../final-input/index.html';
+    window.location.href = '../index.html'
   });
 });
